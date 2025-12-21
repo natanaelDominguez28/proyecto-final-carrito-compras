094fc12b91dc3f0a7d931d73defdf7302e989811
NatanaelDominguez <jorgedominguez349@gmail.com>
2025-12-21 01:22:22 -0300

    aplicación de carrito de compras para el proyecto final del curso React.js dictada por Sergie Code en Digital House

---
+++ src/context/ProductProvider.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react"
+import Swal from "sweetalert2"
+import { ProductContext } from "./ProductContext"
+
+export const ProductProvider = ({children}) => {
+
+    const [products, setProducts] = useState([])
+
+    const fetchProducts = async () => {
+        try {
+        const response = await fetch('https://fakestoreapi.com/products')
+        const data = await response.json()
+        setProducts(data)
+        } catch (error) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Hubo un problema al cargar los productos.'
+        })
+        console.error(error);
+        }
+        
+    }
+
+    useEffect(() => {
+        fetchProducts()
+    }, [])
+
+  return (
+    <ProductContext.Provider value={{products}}>
+        {children}
+    </ProductContext.Provider>
+  )
+}
